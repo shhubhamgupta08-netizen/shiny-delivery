12c1f35f606226d5b2519c5755fa1ae9a59152ff
shhubhamgupta08-netizen <shhubhamgupta08@gmail.com>
2026-05-01 10:35:19 +0530

    Replace App component with CustomClockApp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './CustomClockApp';
 import './index.css';
+import CustomClockApp from './CustomClockApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <CustomClockApp />
   </React.StrictMode>
 );
