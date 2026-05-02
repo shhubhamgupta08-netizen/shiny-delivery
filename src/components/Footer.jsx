8bdaeb74d464813b53d86d483f28968c0b2f1dbb
shhubhamgupta08-netizen <shhubhamgupta08@gmail.com>
2026-05-03 02:53:39 +0530

    Update footer logo source path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -84,7 +84,7 @@ export default function Footer() {
                     {/* Brand */}
                     <div className="md:col-span-4 lg:col-span-5">
                         <img
-                            src="/logo.png"
+                            src="/logo.1png"
                             alt="InstaShiny"
                             className="h-16 md:h-20 w-auto"
                             data-testid="footer-logo"
