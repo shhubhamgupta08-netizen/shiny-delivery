2480ff2f536b44d6b7bdaa24ecbb38dad1dc7c97
shhubhamgupta08-netizen <shhubhamgupta08@gmail.com>
2026-05-03 11:21:25 +0530

    Fix logo image source path in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -84,7 +84,7 @@ export default function Footer() {
                     {/* Brand */}
                     <div className="md:col-span-4 lg:col-span-5">
                         <img
-                            src="/logo.1png"
+                            src="/logo1.png"
                             alt="InstaShiny"
                             className="h-16 md:h-20 w-auto"
                             data-testid="footer-logo"
